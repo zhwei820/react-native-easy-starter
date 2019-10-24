c04795bf86758f0e8cd56a87c0e274f0cc9dd92b
小周 <602068971@qq.com>
2019-10-25 00:00:01 +0800

    stage

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {YellowBox} from 'react-native';
+// import App from "./src605";
+
+import {name as appName} from './app.json';
+
+YellowBox.ignoreWarnings([
+  'Require cycle:',
+  'Remote debugger',
+  'Accessing view manager configs',
+  'Warning: componentWillReceiveProps',
+  'Warning: componentWillMount',
+]);
+
+// import "./debugmodules";
+
+AppRegistry.registerComponent(appName, () => App);
